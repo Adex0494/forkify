6c7a4bd5d18858dd3ff40cdc20e9c1312c27ee29
Ariangel Díaz Espaillat <adex0494@gmail.com>
2021-04-27 14:51:30 -0400

    Listening for load and hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,8 +31,12 @@ const showRecipe = async function () {
   //1) Loading recipe...
   renderSpinner(recipeContainer);
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886 '
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -154,4 +158,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
