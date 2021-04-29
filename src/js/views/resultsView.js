bb0284dbb9147c18743cfd402b312fbf6a2c3a00
Ariangel Díaz Espaillat <adex0494@gmail.com>
2021-04-29 11:09:20 -0400

    Finishing the implementation of search results

---
+++ src/js/views/resultsView.js
@@ -0,0 +1,32 @@
+import View from './View.js';
+
+import icons from 'url:../../img/icons.svg';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query.';
+  _message = '';
+
+  _generateMarkup() {
+    return this._data
+      .map(recipePreview => this._generateRecipePreview(recipePreview))
+      .join('');
+  }
+
+  _generateRecipePreview(recipePreview) {
+    return `<li class="preview">
+            <a class="preview__link" href="#${recipePreview.id}">
+                <figure class="preview__fig">
+                <img src="${recipePreview.image}" alt="${recipePreview.title}" />
+                </figure>
+                <div class="preview__data">
+                    <h4 class="preview__title">${recipePreview.title}.</h4>
+                    <p class="preview__publisher">${recipePreview.publisher}</p>
+                </div>
+            </a>
+        </li>
+        `;
+  }
+}
+
+export default new ResultsView();
