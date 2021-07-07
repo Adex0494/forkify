61fccfd851a27adf1d9aab2270a4c3d62ec15b63
Ariangel Díaz Espaillat <adex0494@gmail.com>
2021-07-07 13:07:52 -0400

    Bug fixed when loading search results

--- src/js/model.js
+++ src/js/model.js
@@ -47,7 +47,7 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
     const data = await AJAX(`${API_URL}?search=${query}`);
-    this.state.search.results = data.data.recipes.map(rec => {
+    state.search.results = data.data.recipes.map(rec => {
       return {
         id: rec.id,
         title: rec.title,
